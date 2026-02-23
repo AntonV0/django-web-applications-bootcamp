0451ef428dc52d57f22825602c5c2c21b5a08c4c
AntonV0 <anton.vlasenko94@gmail.com>
2026-02-23 14:12:06 +0000

    feat: add display employee data feature to CRUDApp

--- first_django_project/CRUDApp/urls.py
+++ first_django_project/CRUDApp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('createdata/', views.createdata, name='createdata'),
+    path('createdata/', views.createdata, name='createData'),
+    path('displayData/', views.get_all_data, name='displayData'),
 ]
--- first_django_project/CRUDApp/views.py
+++ first_django_project/CRUDApp/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from .forms import Empform
+from .models import Emp_detail
+from django.shortcuts import redirect
 
 # Create your views here.
 def createdata(request):
     form = Empform(request.POST)
     if form.is_valid():
         form.save()
-        return render(request, "createform.html")
+        return redirect('displayData')
     return render(request, "createform.html", {'form': form})
+
+def get_all_data(request):
+    data = Emp_detail.objects.all()
+    return render(request, "displayEmp.html", {'data': data})
