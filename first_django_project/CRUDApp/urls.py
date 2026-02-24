be46b47d2380b0a21735a41c8b5dd6ec1c8d36d2
AntonV0 <anton.vlasenko94@gmail.com>
2026-02-24 10:31:55 +0000

    feat: update CRUDApp with 'update' and 'delete' data functions

--- first_django_project/CRUDApp/urls.py
+++ first_django_project/CRUDApp/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('createdata/', views.createdata, name='createData'),
     path('displayData/', views.get_all_data, name='displayData'),
+    path('update/<emp_id>', views.update_data, name='update'),
+    path('delete/<emp_id>', views.delete_data, name='delete')
 ]
--- first_django_project/CRUDApp/views.py
+++ first_django_project/CRUDApp/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import Empform
 from .models import Emp_detail
-from django.shortcuts import redirect
 
 # Create your views here.
 def createdata(request):
@@ -14,3 +13,24 @@ def createdata(request):
 def get_all_data(request):
     data = Emp_detail.objects.all()
     return render(request, "displayEmp.html", {'data': data})
+
+def update_data(request, emp_id):
+    # instead of E_id we can also use pk (primary key) if Ids are 1,2,3... and so on
+    data = Emp_detail.objects.get(E_id=emp_id)
+    form = Empform(instance=data)
+    if request.method == 'POST':
+        form = Empform(request.POST, instance=data)
+        if form.is_valid():
+            form.save()
+            return redirect('displayData')
+    else:
+        form = Empform(instance=data)
+
+    return render(request, "createform.html", {'form': form})
+
+def delete_data(request, emp_id):
+    data = Emp_detail.objects.get(E_id=emp_id)
+    data.delete()
+    return redirect('displayData')
+
+
