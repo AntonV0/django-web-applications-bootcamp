2fd18f71153c75d65798474d6f8108f3386c40f8
AntonV0 <anton.vlasenko94@gmail.com>
2026-02-25 09:58:38 +0000

    feat: add CreateView to classViewApp

--- first_django_project/classViewApp/urls.py
+++ first_django_project/classViewApp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("template_view/", views.HomeClassView.as_view(), name="home"),
     path("list_view/", views.EmployeeListView.as_view(), name="employee_list"),
+    path("create_view/", views.EmployeeCreateView.as_view(), name="employee_create"),
 ]
--- first_django_project/classViewApp/views.py
+++ first_django_project/classViewApp/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, CreateView
 from .models import Emp_class_view_model
+from django.urls import reverse_lazy
 
 class HomeClassView(TemplateView):
     template_name = 'home.html'
@@ -16,3 +17,17 @@ class EmployeeListView(ListView):
     model = Emp_class_view_model
     template_name = 'employeeClass.html'
     context_object_name = 'employees'
+
+class EmployeeCreateView(CreateView):
+    model = Emp_class_view_model
+    fields = ['name', 'age', 'place']
+    template_name = 'employeeCreate.html'
+    success_url = reverse_lazy('employee_list')  # Redirect to the employee list view after successful creation
+
+# redirect(), reverse(), reverse_lazy() are used to redirect to a different URL after a successful operation.
+# reverse_lazy() is used in class-based views because it allows us to reference the URL pattern
+# by its name without needing to import the URL configuration at the top of the file. This is particularly useful
+# when the URL patterns are defined in a different module or when there are circular imports. reverse is used in
+# function-based views where we can directly import the URL configuration and reference the URL pattern by its name.
+# redirect() is used to perform an immediate redirect to a different URL, while reverse() and reverse_lazy() are
+# used to generate the URL based on the URL pattern name.
